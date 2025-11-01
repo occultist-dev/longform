1e2acb53578961ca77d6e9500d864fdb6380ad18
Matthew Quinn <iam@matthewquinn.me>
2025-11-01 23:35:25 +1300

    Documents longform directives

---
+++ serve.ts
@@ -0,0 +1,42 @@
+import { watchFile } from 'fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile, writeFile } from 'node:fs/promises';
+import { createServer } from 'node:http';
+
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const spec = resolve(dir, 'spec/intro.lf');
+const mod = resolve(dir, 'lib/longform.ts');
+
+async function writeHTML() {
+  const { longform } = await import(mod);
+  const doc = await readFile(spec, 'utf-8');
+  const output = longform(doc);
+
+  await writeFile(resolve(dir, 'index.html'), output.root);
+
+  console.log('Updated');
+}
+
+watchFile(spec, async () => {
+  console.log('Changed', spec);
+  await writeHTML();
+});
+console.log('Watching', spec);
+
+watchFile(mod, async () => {
+  console.log('Changed', mod);
+  await writeHTML();
+});
+console.log('Watching', mod);
+
+const server = createServer(async (_req, res) => {
+  const file = await readFile(resolve(dir, 'index.html'));
+  res.writeHead(200, {
+    "content-type": 'text/html',
+  });
+  res.end(file);
+});
+
+server.listen(8080, '127.0.0.1');
